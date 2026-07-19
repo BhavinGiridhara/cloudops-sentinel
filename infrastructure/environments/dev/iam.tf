ea6ce4e74dff12812e72612fd9dd0c1d855b87ac
Bhavin Giridhara <bhavinshriram@gmail.com>
2026-07-18 20:33:18 -0500

    Add Lambda data access policy

--- infrastructure/environments/dev/iam.tf
+++ infrastructure/environments/dev/iam.tf
@@ -25,3 +25,41 @@ resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
 output "lambda_execution_role_arn" {
   value = aws_iam_role.lambda_execution_role.arn
 }
+
+resource "aws_iam_role_policy" "lambda_data_access" {
+  name = "cloudops-sentinel-dev-lambda-data-access"
+  role = aws_iam_role.lambda_execution_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Sid    = "IncidentQueueAccess"
+        Effect = "Allow"
+
+        Action = [
+          "sqs:SendMessage",
+          "sqs:ReceiveMessage",
+          "sqs:DeleteMessage",
+          "sqs:GetQueueAttributes"
+        ]
+
+        Resource = aws_sqs_queue.incident_queue.arn
+      },
+      {
+        Sid    = "IncidentTableAccess"
+        Effect = "Allow"
+
+        Action = [
+          "dynamodb:PutItem",
+          "dynamodb:GetItem",
+          "dynamodb:UpdateItem",
+          "dynamodb:Query"
+        ]
+
+        Resource = aws_dynamodb_table.incidents.arn
+      }
+    ]
+  })
+}
